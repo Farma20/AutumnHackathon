2293aca7721dd162154427cc962778b893cd1970
Farma20 <kustov.ius@students.dvfu.ru>
2023-11-18 20:23:23 +1000

    added splash screen

--- app/src/main/java/com/example/autumnhackathon/ui/MainActivity.kt
+++ app/src/main/java/com/example/autumnhackathon/ui/MainActivity.kt
@@ -1,8 +1,10 @@
-package com.example.autumnhackathon
+package com.example.autumnhackathon.ui
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.autumnhackathon.ui.theme.AutumnHackathonTheme
 import com.example.autumnhackathon.ui.theme.backgroundColor
 import com.example.msmgrouptest.ui.sing_in.SingInScreen
@@ -19,8 +22,20 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepVisibleCondition{
+                viewModel.isLoading.value
+            }
+        }
+
+        //SOFT_INPUT_KEYBOARD
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
         setContent {
             val systemUiController = rememberSystemUiController()
             SideEffect {
@@ -28,7 +43,6 @@ class MainActivity : ComponentActivity() {
                 systemUiController.setNavigationBarColor(color = backgroundColor)
             }
             SingInScreen()
-
         }
     }
 }
